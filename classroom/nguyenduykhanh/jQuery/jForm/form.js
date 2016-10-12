dd014558c1e3e0e2e3c6b9c3c6c3e0f09e6d0c16
Duy Khanh <khanh.nguyenduy@euroland.com>
2016-10-12 14:46:11 +0700

    form js edit

--- classroom/nguyenduykhanh/jQuery/jForm/form.js
+++ classroom/nguyenduykhanh/jQuery/jForm/form.js
@@ -15,16 +15,21 @@ function populateForm(enterLength) {
     if(document.mainForm.output.value = generatePass(enterLength)) {
         $("#rePassword").attr("disabled", "disabled");
         $("#rePassword").css("background", "gray");
-    } else {
-        $("#rePassword").removeAttr("disabled");
     }
 }
 
+if ($("#password").keydown(function(event){
+     $("#rePassword").removeAttr("disabled");
+     $("#rePassword").css("background", "black");
+}));
 
 
 
-//SAVING
-function savings() {
+
+$("#myForm").submit(function(e) {
+    e.preventDefault();
+
+
     var emailRex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
     var errList = ["* Can not leave blank", "* Your email should follow the format name@domain.com", "* Your password should contains one of Uppaercases, !@#$%^&*()_+ and numbers", "* Password is not match"];
     var emailField = $("#email").val();
@@ -47,40 +52,32 @@ function savings() {
         $("#mailError").text(errList[1]);
          $("#mailError").addClass("invalid");
             passInput.focus();
+    } else if (rePassField !== passwordField) {
+        $("#mailError").text(errList[3])
+         $("#mailError").addClass("invalid");
     } else {
+        messege = $('input[name=email]').val();
+        console.log('You put: ' + messege);
+
+
+
         $("#Register").css("color", "cyan");
         $("#mailError").text("");
         var saveText = "processing...";
         var successText = "success!!!"
         $(regisBtn).val(saveText);
+
     setTimeout(function() {
            $(regisBtn).val(successText);
-    }, 2000);
 
-}
-}
+    }, 1800);
 
-
-var email = $("input#email").val();
-var password = $("input#password").val();
-
-var dataString = 'email = ' + email + '&password' + password;
-
-
-$.ajax({
-    type: POST,
-    url: "user.txt",
-    data: dataString,
-    success: function() {
-        $('#contact_form').html("<div id='message'></div>");
-        $('#message').html("<h2>Contact Form Submitted!</h2>")
-        .append("<p>We will be in touch soon.</p>")
-        .hide()
-        .fadeIn(1500, function() {
-          $('#message').append("user.txt");
-    });
-    }
+        setTimeout(function() {
+             window.location.href= "form.html";
+        }, 2000);
+ }
 });
+
 
 
 
