b8d1944808f07a5bd802fc9c5e8aa98e89d8fa84
haconglinh1990 <haconglinh1990@gmail.com>
2016-10-12 17:26:57 +0700

    exercise 3

--- classroom/haconglinh/homeWork7-Lab18-Three exercise/scripts/ex03.js
+++ classroom/haconglinh/homeWork7-Lab18-Three exercise/scripts/ex03.js
@@ -1,26 +1,43 @@
-$(document).ready(function(){  // do not delete 
-// ----------------------------------------------------------------------------
-// EXERCISE 3
-// In the HTML file, first look at the code to see what has been hidden 
-// with this jQuery.
-// Then figure out how to make the appropriate character appear when we
-// hover on his or her name in the main text.
-//
-// Hint: From Code School, do you remember this?
-// var amount = $(this).closest('.vacation').data('price');
-// ----------------------------------------------------------------------------
+$(document).ready(function () { // do not delete 
+    // ----------------------------------------------------------------------------
+    // EXERCISE 3
+    // In the HTML file, first look at the code to see what has been hidden 
+    // with this jQuery.
+    // Then figure out how to make the appropriate character appear when we
+    // hover on his or her name in the main text.
+    //
+    // Hint: From Code School, do you remember this?
+    // var amount = $(this).closest('.vacation').data('price');
+    // ----------------------------------------------------------------------------
 
 
-var rama = $('#rama');
-var sita = $('#sita');
-var hanuman = $('#hanuman');
-var ravana = $('#ravana');
-var lakshmana = $('#lakshmana');
+    var rama = $('#rama');
+    var sita = $('#sita');
+    var hanuman = $('#hanuman');
+    var ravana = $('#ravana');
+    var lakshmana = $('#lakshmana');
 
-$('.character').hide();
-$('.right').prepend("<p>Roll over a character's name to learn more.</p>");
+    $('.character').hide();
+    $('.right').prepend("<p>Roll over a character's name to learn more.</p>");
+    
+//    $('[data-person=rama]')
+    
+    
 
 
+    $('.name').mouseenter(function () {
+        
+            name = $(this).attr('data-person');
+            $(name+'.character').show();
 
-// ----------------------------------------------------------------------------
+    });
+
+    $('.name').mouseleave(function () {
+        
+            name = $(this).attr('data-person');
+            $(name+'.character').hide();
+
+    });
+
+    // ----------------------------------------------------------------------------
 }); // do not delete
