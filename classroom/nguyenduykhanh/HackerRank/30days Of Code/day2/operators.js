a85bb0e8e96af4dbb6e385dc7dbb69d96eeea9ad
Duy Khanh <khanh.nguyenduy@euroland.com>
2016-10-12 21:11:59 +0700

    Hacker Rank solution day 2

--- classroom/nguyenduykhanh/HackerRank/30days Of Code/day2/operators.js
+++ classroom/nguyenduykhanh/HackerRank/30days Of Code/day2/operators.js
@@ -1,1 +1,14 @@
 // Given meal price, tip percent, tax percent. Find and print the metal's total cost
+function totalCost (meal, tip, tax) {
+    var totalCost;
+    this.meal = parseFloat(meal);
+    this.tip = parseInt(tip);
+    this.tax = parseInt(tax);
+    
+    var tipPercent = meal * (tip/100);
+    var taxPercent = meal * (tax/100);
+    
+    totalCost = Math.round(meal + tipPercent + taxPercent); 
+    console.log("The total meal cost is " + totalCost + " dollars");
+   
+}
