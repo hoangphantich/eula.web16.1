bcf759f847b5f7f9e56344dacd85c8cb4b3ae778
haconglinh1990 <haconglinh1990@gmail.com>
2016-10-13 15:44:01 +0700

    Fix Lab 17

--- classroom/haconglinh/homeWork6-Lab17-RegisterFormWithJquery/script.js
+++ classroom/haconglinh/homeWork6-Lab17-RegisterFormWithJquery/script.js
@@ -1,28 +1,34 @@
-function login(){
+(function ($) {
 
-	var email = document.getElementById("userEmail").value;
-	var password = document.getElementById("userPassword").value;
-    var passHash = md5(password);
+    // Config time carousel
+    $(document).ready(function () {
+        $('.carousel').carousel({
+            interval: 1000
+        });
+    });
 
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var obj = JSON.parse(this.responseText);
+    // Send data to sever and check result
+    $('#btnLogin').click(function () {
+        var email = $('#userEmail').val();
+        var password = $('#userPassword').val();
+        var passHash = md5(password);
 
+        $.ajax({
+            url: 'user.json',
+            type: 'POST',
+            asyn: 'true',
+            success: function (result) {
+                $(result.users).each(function (i) {
+                    console.log(result.users.length);
+                    if (email === result.users[i].email && passHash === result.users[i].password) {
+                        alert("Login success");
+                    } else if (i === (result.users.length - 1)) {
+                        alert("Wong username or password");
+                    }
 
+                });
+            }
+        });
+    });
 
-			var n = obj.users.length;
-
-
-			for (var i = 0; i < n; i++) {
-				if (email === obj.users[i].email && passHash === obj.users[i].password) {
-					alert("Login success");
-                    console.log(obj.users[i].email);
-            console.log(obj.users[i].password);
-				}
-			}
-		}
-	};
-	xhttp.open("POST", "user.json", true);
-	xhttp.send();
-}
+}(jQuery));
