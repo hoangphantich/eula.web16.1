a8ea9faed2a5ca533b18374cafefa871c0ec548d
haconglinh1990 <haconglinh1990@gmail.com>
2016-10-14 10:06:51 +0700

    fix wrong exercise 17

--- classroom/haconglinh/homeWork6-Lab17-RegisterFormWithJquery/js/script.js
+++ classroom/haconglinh/homeWork6-Lab17-RegisterFormWithJquery/js/script.js
@@ -1,99 +1,65 @@
 (function ($) {
-    
+
     //Hide all the alert
     $('.alert').hide();
-    
-    var resultUserName, resultEmail, resultPassword, resultRePassword;
 
-    function validUserName() {
-        
-        console.log($('userName').val());
-        
-        //console.log($.trim($('userName').val()));
-//        
-//        
-//        if ($.trim($('userName').val()).length < 6) {
-//            $('userName').addClass('vibrate');
-//            $('#wrongUserName').show();
-//        }else{
-//            $('#wrongUserName').hide();
-//        }
-    }
+    //Check valid user name
+    $('#userName').blur(function () {
 
-    $('userName').blur(validUserName());
-    
-    
-    function validEmail() {
+        if ($.trim($('#userName').val()).length < 6) {
+            $('#userName').addClass('vibrate');
+            $('#wrongUserName').show();
+        } else {
+            $('#wrongUserName').hide();
+        }
+    });
+
+    //Check valid email
+    $('#email').blur(function () {
+
         var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (!filter.test(email.value)) {
-            email.classList.add("vibrate");
-            var alert = document.createElement('span');
-            var textAlert = document.createTextNode("Wrong Email Syntax !!!");
-            alert.setAttribute('class', 'help-block');
-            alert.appendChild(textAlert);
-            document.getElementById("formEmail").appendChild(alert);
-            setTimeout(function () {
-                email.classList.remove("vibrate");
-            }, 250);
-            setTimeout(function () {
-                document.getElementById("formEmail").removeChild(alert);
-            }, 1000);
-            resultEmail = false;
-        } else {
-            resultEmail = true;
-        }
-    }
 
-    $('email').blur(validEmail());
-    
-    
-    function validPassword() {
+        if (!filter.test($.trim($('#email').val()))) {
+            $('#email').addClass('vibrate');
+            $('#wrongEmail').show();
+        } else {
+            $('#wrongEmail').hide();
+        }
+    });
+
+    //Check valid password
+    $('#password').blur(function () {
+
         var filter = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]))/;
-        if (!filter.test(password.value)) {
-            password.classList.add("vibrate");
-            var alert = document.createElement('span');
-            var textAlert = document.createTextNode("Wrong PassWord Syntax !!!");
-            alert.setAttribute('class', 'help-block');
-            alert.appendChild(textAlert);
-            document.getElementById("formPassword").appendChild(alert);
-            setTimeout(function () {
-                password.classList.remove("vibrate");
-            }, 250);
-            setTimeout(function () {
-                document.getElementById("formPassword").removeChild(alert);
-            }, 1000);
-            resultPassword = false;
-        } else {
-            resultPassword = true;
-        }
-    }
 
-    function validRePassword() {
-        if (rePassword.value !== document.getElementById("password").value) {
-            rePassword.classList.add("vibrate");
-            var alert = document.createElement('span');
-            var textAlert = document.createTextNode("Two PassWord are not the same !!!");
-            alert.setAttribute('class', 'help-block');
-            alert.appendChild(textAlert);
-            document.getElementById("formRePassword").appendChild(alert);
-            setTimeout(function () {
-                password.classList.remove("vibrate");
-            }, 250);
-            setTimeout(function () {
-                document.getElementById("formRePassword").removeChild(alert);
-            }, 1000);
-            document.getElementById("password").focus();
-            resultRePassword = false;
+        if (!filter.test($.trim($('#password').val()))) {
+            $('#password').addClass('vibrate');
+            $('#wrongPassword').show();
         } else {
-            resultRePassword = true;
+            $('#wrongPassword').hide();
         }
-    }
+    });
 
-    function register() {
-        if ((resultUserName === false) || (resultEmail === false) || (resultPassword === false) || (resultRePassword === false)) {
-            alert("Wrong Syntax, please check back");
+    //Check valid Re-Password
+    $('#rePassword').blur(function () {
+
+        if ($.trim($('#password').val()) !== $.trim($('#rePassword').val())) {
+            $('#rePassword').addClass('vibrate');
+            $('#wrongRePassword').show();
         } else {
-            window.location.replace("result.html");
+            $('#wrongRePassword').hide();
         }
-    }
+    });
+
+    //Button Register
+    $('#register').click(function () {
+
+        if ($('.alert').is(':visible')) {
+            alert("Wrong Syntax, please re-check back");
+        } else {
+            alert("Register sucessfull");
+        }
+
+    });
+
 }(jQuery));
