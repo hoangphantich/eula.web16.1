a3d76759282f54a7914ebfd7422bd1faa94095c7
haconglinh1990 <haconglinh1990@gmail.com>
2016-10-17 17:11:47 +0700

    Add Lab 19 (not finish)

--- classroom/haconglinh/homeWork9-Lab20-TodoList/js/script.js
+++ classroom/haconglinh/homeWork9-Lab20-TodoList/js/script.js
@@ -1,54 +1,218 @@
-(function ($) {
+var todo = todo || {},
+    data = JSON.parse(localStorage.getItem("todoData"));
 
-    //Slider
-    $(window).load(function () {
-        $("#slider").twentytwenty({
-            default_offset_pct: 0.85
+data = data || {};
+
+(function (todo, data, $) {
+
+    var defaults = {
+            todoTask: "todo-task",
+            todoHeader: "task-header",
+            todoDate: "task-date",
+            todoDescription: "task-description",
+            taskId: "task-",
+            formId: "todo-form",
+            dataAttribute: "data",
+            deleteDiv: "delete-div"
+        },
+        codes = {
+            "1": "#pending",
+            "2": "#inProgress",
+            "3": "#completed"
+        };
+
+    todo.init = function (options) {
+
+        options = options || {};
+        options = $.extend({}, defaults, options);
+
+        $.each(data, function (index, params) {
+            generateElement(params);
         });
-    });
 
+        // Adding drop function to each category of task
+        $.each(codes, function (index, value) {
+            $(value).droppable({
+                drop: function (event, ui) {
+                    var element = ui.helper,
+                        css_id = element.attr("id"),
+                        id = css_id.replace(options.taskId, ""),
+                        object = data[id];
 
-    //TypeHead
-    var substringMatcher = function (strs) {
-        return function findMatches(q, cb) {
-            var matches, substringRegex;
+                    // Removing old element
+                    removeElement(object);
 
-            // an array that will be populated with substring matches
-            matches = [];
+                    // Changing object code
+                    object.code = index;
 
-            // regex used to determine if a string contains the substring `q`
-            substrRegex = new RegExp(q, 'i');
+                    // Generating new element
+                    generateElement(object);
 
-            // iterate through the pool of strings and for any string that
-            // contains the substring `q`, add it to the `matches` array
-            $.each(strs, function (i, str) {
-                if (substrRegex.test(str)) {
-                    matches.push(str);
+                    // Updating Local Storage
+                    data[id] = object;
+                    localStorage.setItem("todoData", JSON.stringify(data));
+
+                    // Hiding Delete Area
+                    $("#" + defaults.deleteDiv).hide();
                 }
             });
+        });
 
-            cb(matches);
-        };
+        // Adding drop function to delete div
+        $("#" + options.deleteDiv).droppable({
+            drop: function (event, ui) {
+                var element = ui.helper,
+                    css_id = element.attr("id"),
+                    id = css_id.replace(options.taskId, ""),
+                    object = data[id];
+
+                // Removing old element
+                removeElement(object);
+
+                // Updating local storage
+                delete data[id];
+                localStorage.setItem("todoData", JSON.stringify(data));
+
+                // Hiding Delete Area
+                $("#" + defaults.deleteDiv).hide();
+            }
+        })
+
     };
 
-    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
+    // Add Task
+    var generateElement = function (params) {
+        var parent = $(codes[params.code]),
+            wrapper;
 
-    $('#the-basics .typeahead').typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-    }, {
-        name: 'states',
-        source: substringMatcher(states)
+        if (!parent) {
+            return;
+        }
+
+        wrapper = $("<div />", {
+            "class": defaults.todoTask,
+            "id": defaults.taskId + params.id,
+            "data": params.id
+        }).appendTo(parent);
+
+        $("<div />", {
+            "class": defaults.todoHeader,
+            "text": params.title
+        }).appendTo(wrapper);
+
+        $("<div />", {
+            "class": defaults.todoDate,
+            "text": params.date
+        }).appendTo(wrapper);
+
+        $("<div />", {
+            "class": defaults.todoDescription,
+            "text": params.description
+        }).appendTo(wrapper);
+
+        wrapper.draggable({
+            start: function () {
+                $("#" + defaults.deleteDiv).show();
+            },
+            stop: function () {
+                $("#" + defaults.deleteDiv).hide();
+            },
+            revert: "invalid",
+            revertDuration: 200
+        });
+
+    };
+
+    // Remove task
+    var removeElement = function (params) {
+        $("#" + defaults.taskId + params.id).remove();
+    };
+
+    todo.add = function () {
+        var inputs = $("#" + defaults.formId + " :input"),
+            errorMessage = "Title can not be empty",
+            id, title, description, date, tempData;
+
+        if (inputs.length !== 4) {
+            return;
+        }
+
+        title = inputs[0].value;
+        description = inputs[1].value;
+        date = inputs[2].value;
+
+        if (!title) {
+            generateDialog(errorMessage);
+            return;
+        }
+
+        id = new Date().getTime();
+
+        tempData = {
+            id: id,
+            code: "1",
+            title: title,
+            date: date,
+            description: description
+        };
+
+        // Saving element in local storage
+        data[id] = tempData;
+        localStorage.setItem("todoData", JSON.stringify(data));
+
+        // Generate Todo Element
+        generateElement(tempData);
+
+        // Reset Form
+        inputs[0].value = "";
+        inputs[1].value = "";
+        inputs[2].value = "";
+    };
+
+    var generateDialog = function (message) {
+        var responseId = "response-dialog",
+            title = "Messaage",
+            responseDialog = $("#" + responseId),
+            buttonOptions;
+
+        if (!responseDialog.length) {
+            responseDialog = $("<div />", {
+                title: title,
+                id: responseId
+            }).appendTo($("body"));
+        }
+
+        responseDialog.html(message);
+
+        buttonOptions = {
+            "Ok": function () {
+                responseDialog.dialog("close");
+            }
+        };
+
+        responseDialog.dialog({
+            autoOpen: true,
+            width: 400,
+            modal: true,
+            closeOnEscape: true,
+            buttons: buttonOptions
+        });
+    };
+
+    todo.clear = function () {
+        data = {};
+        localStorage.setItem("todoData", JSON.stringify(data));
+        $("." + defaults.todoTask).remove();
+    };
+
+    $("#taskDate").datepicker();
+    $("#taskDate").datepicker("option", "dateFormat", "dd/mm/yy");
+
+    $(".task-container").droppable();
+    $("#pending").draggable({
+        revert: "valid",
+        revertDuration: 200
     });
+    todo.init();
 
-}(jQuery));
+})(todo, data, jQuery);
